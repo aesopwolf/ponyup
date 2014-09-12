ef4337b0495225a8fea86a3c9f36e90ea44921d1
Aesop Wolf <aesopwolf@gmail.com>
2014-09-12 14:58:10 -0700

    ui: designed listing page

--- public/custom.js
+++ public/custom.js
@@ -5,6 +5,106 @@ var app = angular.module('app', [
   'ngFitText',
   'angular-loading-bar'
 ])
+.config(function($stateProvider, $locationProvider, $uiViewScrollProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+  $uiViewScrollProvider.useAnchorScroll()
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'partials/index.html'
+    })
+    .state('faq', {
+      url: '/faq',
+      templateUrl: 'partials/faq.html'
+    })
+    .state('cause', {
+      url: '/cause',
+      templateUrl: 'partials/cause.html',
+      controller: 'causeCtrl'
+    })
+    .state('demo', {
+      url: '/demo',
+      templateUrl: 'partials/demo.html',
+      controller: 'demoCtrl'
+    })
+    .state('demo.active', {
+      templateUrl: 'partials/cause.html'
+    });
+})
+.run(function($location) {
+
+})
+.controller("demoCtrl", function($scope, $state) {
+  angular.element('.navbar-wrapper').addClass('hidden');
+  $state.go('demo.active');
+})
+.controller("causeCtrl", function($rootScope, $scope, $http) {
+
+})
+.controller("mainCtrl", function($rootScope, $scope, $http, $location) {
+  // hide the navigation bar for certain pages
+  $rootScope.$on('$stateChangeSuccess', function() {
+    if($location.path() == '/cause' || $location.path() == '/demo') {
+      angular.element('.navbar-wrapper').addClass('hidden');
+    }
+    else {
+      angular.element('.navbar-wrapper').removeClass('hidden');
+    }
+  });
+
+  $scope.cause = {};
+  $scope.cause.items = [{placeholder: "Pizza", placeholderPrice: "$20"}, {placeholder: "Soda", placeholderPrice: "$5"}];
+  $scope.totalPrice = 0;
+  $scope.isFocused = false;
+  $scope.isCollapsed = true;
+  $scope.loading = false;
+
+  $scope.add = function() {
+    $scope.isFocused = false;
+    $scope.cause.items.push({});
+    setTimeout(function() {
+      $scope.$apply(function() {
+        $scope.isFocused = true;
+      });
+    }, 100);
+  }
+
+  $scope.remove = function(data) {
+    $scope.cause.items.splice(data, 1);
+  };
+
+  $scope.$watch("cause.items", function(newValue, oldValue) {
+    $scope.totalPrice = 0;
+    angular.forEach($scope.cause.items, function(value, key) {
+      if(parseFloat(value.price, 10)) {
+        $scope.totalPrice += parseFloat(value.price, 10);
+      }
+    })
+  }, true)
+
+  $scope.submit = function() {
+    $scope.loading = true;
+    $scope.errorMessage = false;
+    $http.post('api/cause', $scope.cause)
+    .success(function(data) {
+      $scope.loading = false;
+    })
+    .error(function(data) {
+      $scope.loading = false;
+      $scope.errorMessage = data.message || "You can try refreshing the page.";
+    });
+  }
+
+  $rootScope.pages = 0;
+  $rootScope.$on('$stateChangeStart', function() {
+    $rootScope.pages += 1;
+    if($rootScope.pages >= 2) {
+      $scope.isCollapsed = true;
+    }
+  })
+})
 .filter('url', function ($sce) {
   return function (text) {
     return text?text.replace(/ /g, '-'):'';
@@ -61,75 +161,4 @@ var app = angular.module('app', [
       });
     }
   };
-})
-.config(function($stateProvider, $locationProvider, $uiViewScrollProvider, $urlRouterProvider) {
-  $locationProvider.html5Mode(true);
-  $uiViewScrollProvider.useAnchorScroll()
-  $urlRouterProvider.otherwise("/");
-
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "partials/index.html"
-    })
-    .state('faq', {
-      url: "/faq",
-      templateUrl: "partials/faq.html"
-    })
-    .state('demo', {
-      url: "/demo",
-      templateUrl: "partials/cause.html"
-    });
-})
-.controller("main", function($rootScope, $scope, $http) {
-  $scope.cause = {};
-  $scope.cause.items = [{placeholder: "Pizza", placeholderPrice: "$20"}, {placeholder: "Soda", placeholderPrice: "$5"}];
-  $scope.totalPrice = 0;
-  $scope.isFocused = false;
-  $scope.isCollapsed = true;
-  $scope.loading = false;
-
-  $scope.add = function() {
-    $scope.isFocused = false;
-    $scope.cause.items.push({});
-    setTimeout(function() {
-      $scope.$apply(function() {
-        $scope.isFocused = true;
-      });
-    }, 100);
-  }
-
-  $scope.remove = function(data) {
-    $scope.cause.items.splice(data, 1);
-  };
-
-  $scope.$watch("cause.items", function(newValue, oldValue) {
-    $scope.totalPrice = 0;
-    angular.forEach($scope.cause.items, function(value, key) {
-      if(parseFloat(value.price, 10)) {
-        $scope.totalPrice += parseFloat(value.price, 10);
-      }
-    })
-  }, true)
-
-  $scope.submit = function() {
-    $scope.loading = true;
-    $scope.errorMessage = false;
-    $http.post('api/cause', $scope.cause)
-    .success(function(data) {
-      $scope.loading = false;
-    })
-    .error(function(data) {
-      $scope.loading = false;
-      $scope.errorMessage = data.message || "You can try refreshing the page.";
-    });
-  }
-
-  $rootScope.pages = 0;
-  $rootScope.$on('$stateChangeStart', function() {
-    $rootScope.pages += 1;
-    if($rootScope.pages >= 2) {
-      $scope.isCollapsed = true;
-    }
-  })
 });
