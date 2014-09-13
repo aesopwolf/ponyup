222b4226ab3a42fa0c7635d4a1808f4a7b101c33
Aesop Wolf <aesopwolf@gmail.com>
2014-09-13 15:00:52 -0700

    ux: added interactive demo

--- public/custom.js
+++ public/custom.js
@@ -37,10 +37,52 @@ var app = angular.module('app', [
 .run(function($location) {
 
 })
+.animation(".fakeMouse", function() {
+  return {
+    leave: function(element, done) {
+      
+    },
+    enter: function(element, done) {
+      var starting = {
+        top: element[0].offsetTop,
+        left: element[0].offsetLeft
+      };
+
+      var ending = {
+        top: angular.element("#cause")[0].offsetTop,
+        left: angular.element("#cause")[0].offsetLeft,
+      };
+
+      var point1 = {
+        top: ending.top - starting.top,
+        left: ending.left - starting.left
+      };
+
+      var point2 = {
+        top: angular.element("#item0").offset().top - starting.top,
+        left: angular.element("#item0").offset().left - starting.left
+      };
+
+      var point3 = {
+        top: angular.element("#item1").offset().top - starting.top,
+        left: angular.element("#item1").offset().left - starting.left
+      };
+
+      TweenMax.fromTo(element, 0.5, {x: 0, y: 0}, {x: point1.left - 4, y: point1.top + 15});
+      TweenMax.fromTo(element, 0.25, {x: point1.left - 4, y: point1.top + 15}, {x: point2.left - 4, y: point2.top + 18, delay: 0.8});
+      TweenMax.fromTo(element, 0.25, {x: point2.left - 4, y: point2.top + 18}, {x: point3.left - 4, y: point3.top + 18, delay: 1.4, onComplete: done});
+    }
+  }
+})
 .controller("demoCtrl", function($state) {
   $state.go('demo.active');
 })
 .controller("causeCtrl", function($scope, $modal) {
+  $scope.cause = {};
+  $scope.cause.name = "Summer BBQ at the beach";
+  $scope.cause.description = "Hey guys! We're having an end of summer beach party on September 13th. I already bought all of the supplies below. I'd appreciate it if you pitch in some $$ Thanks :)";
+  $scope.cause.items = [{description: "Hamburgers and hotdogs", price: 22.56}, {description: "Drinks and ice", price: 12.56}, {description: "Plates/napkins/charcoal/lighter fluid", price: 15.16}];
+
   var handler = StripeCheckout.configure({
     key: 'pk_test_854lbQWakbhRqcBFPcjRfXfx',
     token: function(token) {
@@ -61,15 +103,72 @@ var app = angular.module('app', [
   $scope.getCC = function() {
     handler.open({
       name: 'Sarah Fox',
-      description: 'Kimberly\'s Surprise Birthday BBQ',
+      description: 'Summer BBQ at the beach',
       amount: $scope.dollarAmount * 100
     });
   }
 })
-.controller("mainCtrl", function($rootScope, $scope, $http, $location) {
+.controller("mainCtrl", function($rootScope, $scope, $http, $location, $timeout, $state) {
+  $rootScope.playDemo = false;
+  $scope.cause = {};
+  $scope.cause.name = '';
+  $scope.cause.items = [{placeholder: "Pizza", placeholderPrice: "$20"}, {placeholder: "Soda", placeholderPrice: "$5"}];
+
+  $scope.totalPrice = 0;
+  $scope.isFocused = false;
+  $scope.isCollapsed = true;
+  $scope.loading = false;
+
+  var typingAnimation = function(scope, text) {
+    eval(scope + " = '';");
+    var counter = 0;
+    var interval = setInterval(function() {
+      // console.log(counter);
+      $scope.$apply(function() {
+        eval(scope  + " += text.split('')[counter];");
+      });
+      if(counter >= text.split('').length - 1) {
+        clearInterval(interval);
+      }
+      counter ++;
+     }, 10);
+  };
+
+  $scope.demo = function() {
+    $rootScope.playDemo = true;
+
+    $timeout(function() {
+      typingAnimation('$scope.cause.name', "Summer BBQ at the beach");
+      $timeout(function() {
+        typingAnimation('$scope.cause.items[0].description', "Hamburgers and hotdogs");
+        $timeout(function() {
+          $scope.cause.items[0].price = 22.56;
+        }, 200)
+      }, 600);
+      $timeout(function() {
+        typingAnimation('$scope.cause.items[1].description', "Drinks and ice");
+        $timeout(function() {
+          $scope.cause.items[1].price = 12.56;
+        }, 200)
+      }, 1200);
+
+      $timeout(function() {
+        $rootScope.playDemo = false;
+        $state.go('demo');
+      }, 2100);
+    }, 500);
+  };
+
   // hide the navigation bar for certain pages
   $scope.hidden = true;
   $rootScope.$on('$stateChangeSuccess', function() {
+    if($location.path() == '/') {
+      angular.element('.fakeMouse').remove();
+      $scope.cause = {};
+      $scope.cause.name = '';
+      $scope.cause.items = [{placeholder: "Pizza", placeholderPrice: "$20"}, {placeholder: "Soda", placeholderPrice: "$5"}];
+    }
+
     if($location.path() == '/cause' || $location.path() == '/demo') {
       angular.element('.navbar-wrapper').addClass('hidden');
     }
@@ -79,16 +178,9 @@ var app = angular.module('app', [
     }
   });
 
-  $scope.cause = {};
-  $scope.cause.lineItems = [{placeholder: "Pizza", placeholderPrice: "$20"}, {placeholder: "Soda", placeholderPrice: "$5"}];
-  $scope.totalPrice = 0;
-  $scope.isFocused = false;
-  $scope.isCollapsed = true;
-  $scope.loading = false;
-
   $scope.add = function() {
     $scope.isFocused = false;
-    $scope.cause.lineItems.push({});
+    $scope.cause.items.push({});
     setTimeout(function() {
       $scope.$apply(function() {
         $scope.isFocused = true;
@@ -97,12 +189,12 @@ var app = angular.module('app', [
   }
 
   $scope.remove = function(data) {
-    $scope.cause.lineItems.splice(data, 1);
+    $scope.cause.items.splice(data, 1);
   };
 
-  $scope.$watch("cause.lineItems", function(newValue, oldValue) {
+  $scope.$watch("cause.items", function(newValue, oldValue) {
     $scope.totalPrice = 0;
-    angular.forEach($scope.cause.lineItems, function(value, key) {
+    angular.forEach($scope.cause.items, function(value, key) {
       if(parseFloat(value.price, 10)) {
         $scope.totalPrice += parseFloat(value.price, 10);
       }
@@ -133,6 +225,11 @@ var app = angular.module('app', [
 .filter('url', function ($sce) {
   return function (text) {
     return text?text.replace(/ /g, '-'):'';
+  };
+})
+.filter('unsafe', function($sce) {
+  return function(val) {
+    return $sce.trustAsHtml(val);
   };
 })
 .directive('format', ['$filter', function ($filter) {
